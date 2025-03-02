2909ad9af86ed28c093ec7a1c58014c977042753
MSI\CJRow <cjrowland05@gmail.com>
2025-03-02 16:25:02 -0500

    camera position early

--- RubixCube/Assets/Scripts/playercont.cs
+++ RubixCube/Assets/Scripts/playercont.cs
@@ -9,6 +9,9 @@ public class playercont : MonoBehaviour
     public float maxspeed;
     public float movespeed;
     public float jump;
+    public GameObject currentbox;
+    public Transform camtarget;
+    public Camera cam;
 
     public void Update()
     {
@@ -30,6 +33,22 @@ public class playercont : MonoBehaviour
         if (RB.velocity.magnitude > maxspeed)
         {
             RB.drag = 4;
+        }
+    }
+
+    public void OnTriggerEnter(Collider other)
+    {
+        currentbox = other.gameObject;
+        StartCoroutine(cameramove());
+    }
+
+    public IEnumerator cameramove()
+    {
+        camtarget = currentbox.transform.Find("Target");
+        while (cam.transform.position!=camtarget.transform.position)
+        {
+            cam.transform.position=Vector3.Lerp(cam.transform.position,camtarget.transform.position,0.2f);
+            yield return new WaitForFixedUpdate();
         }
     }
 }
